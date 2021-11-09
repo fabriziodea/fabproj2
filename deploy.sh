0ca0e79a480b750ddfbff3204d22bfa8c7cb10bf
fabriziodea <fabri1984@gmail.com>
2021-11-09 14:56:24 +0000

    added tests and deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt install python3 python3-pip python3-venv gunicorn3 -y
+
+#python3 -m venv venv
+#source venv/bin/activate
+
+pip3 install -r requirements.txt
+
+#while getops "c" options; do
+#    case ${options} in
+#        c) create=true;;
+#    esac
+#done
+
+#if [ ${create}]; then
+#    python create.py
+#fi
+
+
+#python3 app.py
+
+#python3 -m pytest --cov=application
+python3 -m pytest --cov=Fives
