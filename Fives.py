c61f16e9e57904df1a32a78784c0edc3605b908d
fabriziodea <fabri1984@gmail.com>
2021-11-09 15:49:09 +0000

    if name is main

--- Fives.py
+++ Fives.py
@@ -154,5 +154,5 @@ def deletegame(matchno):
     db.session.commit()
     return redirect("/")
 
-
-app.run(debug=True, host='0.0.0.0')
+if __name__='__main__':
+    app.run(debug=True, host='0.0.0.0')
