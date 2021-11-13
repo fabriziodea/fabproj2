7bfde46fb07d00d4779fdfc75d2a6318771b3377
fabriziodea <fabri1984@gmail.com>
2021-11-13 13:44:25 +0000

    FAB-51 #IN PROGRESS edited test file

--- tests/Fives_test.py
+++ tests/Fives_test.py
@@ -1,6 +1,6 @@
 from flask import url_for
 from flask_testing import TestCase
-from application import app, db
+from app import app, db
 from application.models import Match
 import datetime
 import application.routes
